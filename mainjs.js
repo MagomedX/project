1130af2fea133907e86eb9bf454a774c30c5b02c
MagomedX <maus25_50@mail.ru>
2024-04-07 08:04:15 +0300

    first commit

---
+++ mainjs.js
@@ -0,0 +1,101 @@
+
+/*
+let a = 2;
+let b = 5;
+
+let 1a = "error"
+let -b = "error"
+   
+b = a + b
+b = a - b
+a = a - b
+
+//console.log(a);
+
+let name = "John";
+let admin = name;
+
+alert(admin)
+
+
+//console.log(a); */
+
+//mission ; JavaScript 2
+
+
+/*   let date: number = 28.02.1999;
+let P = matt.P;
+
+let a = 2;
+
+const b = 2;
+
+// b = 3;
+
+console.log(b)
+ 
+let ruble = 100 
+let dollar = 1
+
+alert(' ruble =  ' + ruble + '   dollar =   ' + dollar)
+
+let i_have = prompt ('i have rubles') 
+let i_give = i_have / ruble
+
+alert(i_give)
+
+let name = prompt ('your name?')
+
+let adress = prompt('your adress?')
+
+let phone = prompt('your phone?')
+
+console.log(    
+    `student name: ${name}; 
+    student adress: ${adress};
+    student phone: ${phone} `
+)
+
+*/
+// js mission: 3
+
+/* 1
+const num = prompt("rite number")
+
+if(num < 10){
+    alert( 'constanta < 10')
+} else{
+    alert('constanta > 10')
+} 
+*/
+/* 2
+let y = 7
+let x = 10 < y ? 'x < y' : 'x > y';
+
+console.log(x)
+*/
+
+ /* 3
+let num = prompt('number?')
+
+if(num%2===0){
+    console.log('chet')
+} else{
+    console.log('nechet')
+}
+*/
+
+/* 4
+let userN1 = prompt('your number?')
+let userN2 = prompt('your number 2?')
+let userN3 = prompt('your number 3?')
+
+if(userN1 > userN2 && userN1 > userN3){
+    console.log(userN1)
+} else if(userN2 > userN1 && userN2 > userN3){
+    console.log(userN2)
+} else if(userN3 > userN1 && userN3 > userN2){
+    console.log(userN3)
+}
+*/
+
